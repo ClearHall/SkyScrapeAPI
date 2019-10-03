2e066320bc2465a5891519298732761fc56c2b19
Hunter Han <hunter.han@gmail.com>
2019-10-02 22:30:17 -0500

    Initial Commit

---
+++ test/skyscrapeapi_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:skyscrapeapi/SkywardAPICore.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = SkywardAPICore("https://skyward-fbprod.iscorp.com/scripts/wsisa.dll/WService=wsedufortbendtx/seplog01.w");
+
+  });
+}
